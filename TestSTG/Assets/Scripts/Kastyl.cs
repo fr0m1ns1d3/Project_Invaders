bed69113de974e3394f3a34297a6aeb06f9f5f2b
sTarBlakout <starblackout.channel@gmail.com>
2019-03-10 15:24:28 +0200

    пофиксил меню

---
+++ TestSTG/Assets/Scripts/Kastyl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kastyl : MonoBehaviour
+{
+    private bool active;
+
+    void Start()
+    {
+        active = PauseMenu.GameIsPaused;
+        Time.timeScale = 0f;
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
--- TestSTG/Assets/Scripts/PauseMenu.cs
+++ TestSTG/Assets/Scripts/PauseMenu.cs
@@ -10,11 +10,9 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-        //if (Input.GetButtonDown("PauseButton"))
         {
             if (GameIsPaused)
             {
