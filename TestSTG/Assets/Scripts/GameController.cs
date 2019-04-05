dcb0fdd5e34b5bb8d9187357ee86c9768b82f5d8
sTarBlakout <starblackout.channel@gmail.com>
2019-04-05 20:52:16 +0300

    Добавил немношк больше логики спавна

--- TestSTG/Assets/Scripts/GameController.cs
+++ TestSTG/Assets/Scripts/GameController.cs
@@ -7,17 +7,22 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-
-    public GameObject[] hazards;
+    [SerializeField]
+    private GameObject[] hazards;
+    [SerializeField]
+    private GameObject[] boosts;
     public Vector3 spawnValues;
     public int countHazard;
     public float spawnWait, startWait, waitWave;
 
     public Text scoreText, gameOverText;
     private bool gameOver = false;
-    private int score = 0, hazardID, shipCounter;
+    private int score = 0, hazardID, shipCounter, boostID;
     [SerializeField]
     private GameObject pauseMenu;
+
+    private int boostCounter = 1, boostTempCounter = 0;
+    private static int boostActive = 1;
     
     void Start()
     {
@@ -26,6 +31,7 @@ public class GameController : MonoBehaviour
         StartCoroutine(SpawnWaves());
         gameOverText.text = "";
         shipCounter = 0;
+        boostCounter = 1;
     }
 
     IEnumerator SpawnWaves ()
@@ -34,9 +40,20 @@ public class GameController : MonoBehaviour
         while (!gameOver)
         {
             shipCounter = 0;
+            boostTempCounter = 0;
+
+            if (countHazard > 8 + boostCounter) boostCounter++;
 
             for (int i = 0; i < countHazard; i++)
             {
+                if (boostTempCounter < boostCounter)
+                {
+                    if (Random.Range(1, 5) == boostActive)
+                    {
+                        callBoost();
+                    }
+                }
+
                 hazardID = Random.Range(0, hazards.Length);
 
                 if (hazardID == 5 && shipCounter>=2)
@@ -61,6 +78,16 @@ public class GameController : MonoBehaviour
 
         pauseMenu.SetActive(true);
         gameOver = false;
+    }
+
+    private void callBoost()
+    {
+        boostID = Random.Range(0, boosts.Length);
+        GameObject boost = boosts[boostID];
+        Vector3 boostPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion boostRotation = Quaternion.identity;
+        Instantiate(boost, boostPosition, boostRotation);
+        boostTempCounter++;
     }
 
     public void AddScore(int fixedScore)
